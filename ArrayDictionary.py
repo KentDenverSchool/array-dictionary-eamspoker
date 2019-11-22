3891a49edbb8fa280d9da7268db25347c094209c
Emily Amspoker <eamspoker21@kentdenver.org>
2019-11-22 09:33:26 -0700

    finished writing methods, added tests

---
+++ ArrayDictionary.py
@@ -0,0 +1,44 @@
+import copy
+class ArrayDictionary:
+    def __init__(self):
+        self.keys = []
+        self.values = []
+    #add an key-value pair to the dictionary
+    def put(self, key, value):
+        self.keys.append(key)
+        self.values.append(value)
+        
+    #get the value associated with a given key
+    def get(self, key):
+        return self.values[self.keys.index(key)]
+
+    #remove a key-value pair and return its value
+    def remove(self, key):
+        value = self.values[self.keys.index(key)]
+        self.values.remove(key)
+        return value
+
+    #returns true if the given key has an associated value
+    def contains(self, key):
+        return key in self.keys
+          
+
+    #returns true if the dictionary is empty
+    def isEmpty(self):
+       return not keys and not values
+    
+
+    #returns the number of key-value pairs in the dictionary
+    def size(self):
+        return len(self.keys)
+
+    #returns a list of keys
+    def keys(self):
+        return copy.deepcopy(self.keys)
+
+    #returns a list of values
+    def values(self):
+        return copy.deepcopy(self.values)
+
+
+        
---
+++ ArrayDictionaryDriver.py
@@ -0,0 +1,23 @@
+import ArrayDictionary
+
+
+if __name__ == "main":
+    dictionary = ArrayDictionary.ArrayDictionary()
+    results = "_________ArrayDictionary Tests_________"+
+                "\n\nTests in format Expected: Actual: passed:true/false\n\n"
+    
+    #testing init
+    results += dictionary.isEmpty() + "isEmpty for empty constructor. Expected: true Actual: " +
+        dictionary.isEmpty()
+    results += dictionary.size() + "isEmpty for empty constructor. Expected: true Actual: " +
+        dictionary.size()
+
+    #put/get/contains test
+    dictionary.put("key1", "value1")
+
+    
+                
+            
+    
+    logName = input("Enter in log name:\n")
+    
