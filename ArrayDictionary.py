8cbc48202dbb87fd50dfc8d6a44f7b30cf60f3eb
Emily Amspoker <eamspoker21@kentdenver.org>
2019-11-25 12:55:14 -0700

    Tested name change

--- ArrayDictionary.py
+++ ArrayDictionary.py
@@ -5,7 +5,7 @@ class ArrayDictionary:
         self.keys = []
         self.values = []
     
-    #add an key-value pair to the dictionary
+    #adds an key-value pair to the dictionary, takes in a key and a value 
     def put(self, key, value):
         if key in self.keys:
             self.values[self.keys.index(key)] = value
@@ -13,11 +13,11 @@ class ArrayDictionary:
             self.keys.append(key)
             self.values.append(value)
         
-    #get the value associated with a given key
+    #get the value associated with a given key, takes in a key
     def get(self, key):
         return self.values[self.keys.index(key)]
 
-    #remove a key-value pair and return its value
+    #removes a key-value pair, returns its value, takes in a key
     def remove(self, key):
         if self.contains(key):
             value = self.values[self.keys.index(key)]
@@ -27,26 +27,26 @@ class ArrayDictionary:
         else:
             return null
 
-    #returns true if the given key has an associated value
+    #returns true if the given key has an associated value, takes in a key
     def contains(self, key):
         return key in self.keys
           
 
-    #returns true if the dictionary is empty
+    #returns true if the dictionary is empty, no parameters
     def isEmpty(self):
        return not self.keys and not self.values
     
 
-    #returns the number of key-value pairs in the dictionary
+    #returns the number of key-value pairs in the dictionary, no parameters
     def size(self):
         return len(self.keys)
 
-    #returns a list of keys
-    def listKeys(self):
+    #returns a list of keys, no parameters
+    def getKeys(self):
         return copy.deepcopy(self.keys)
 
-    #returns a list of values
-    def listValues(self):
+    #returns a list of values, no parameters
+    def getValues(self):
         return copy.deepcopy(self.values)
 
 
--- ArrayDictionaryDriver.py
+++ ArrayDictionaryDriver.py
@@ -14,7 +14,7 @@ dictionary.put("key1", "value1")
 results += "\n\nPut 2 key values in the dictionary, change the value of one by putting a new value for the old key\n"
 results += str(dictionary.contains("key1")) + " Contains. Expected true: Actual: " + str(dictionary.contains("key1")) + "\n"
     
-#test if doesn't contain not added key
+#test if doesn't contain not added key and contains added key, check that it isn't empty, size
 results += str(not dictionary.contains("key2")) + " Contains (not added key). Expected: false Actual: " + str(dictionary.contains("key2")) + "\n"
 results += str(dictionary.get("key1") == "value1") + " get. Expected: value1 Actual: " + dictionary.get("key1") + "\n"
 results += str(not dictionary.isEmpty()) + " isEmpty. Expected: false Actual: " + str(dictionary.isEmpty()) + "\n"
@@ -22,7 +22,7 @@ results += str(dictionary.size() == 1) + " size. Expected: 1 Actual: " + str(dic
 
 results += "2nd add:\n"
 dictionary.put("key2", "value2")
-
+#test if contains added key, that is isn't empty, size
 results += str(dictionary.contains("key2")) +  " contains added key. Expected: false Actual: " + str(dictionary.contains("key2")) + "\n"
 results += str(dictionary.get("key2") == "value2") + " get added value. Expected: value2 Actual: " + dictionary.get("key2") + "\n"
 results += str(not dictionary.isEmpty()) + " isEmpty. Expected: false Actual: " + str(dictionary.isEmpty()) + "\n"
@@ -31,8 +31,8 @@ results += str(dictionary.size() == 2) + " size. Expected: 2 Actual: " + str(dic
 dictionary.put("key2", "value3")
 
 results += "Change 2nd value:\n"
-
-results += str(dictionary.contains("key2")) + " contains  added key. Expected true: Actual: " + str(dictionary.contains("key2")) + "\n"
+#test if contains added key, that is isn't empty, size
+results += str(dictionary.contains("key2")) + " contains added key. Expected true: Actual: " + str(dictionary.contains("key2")) + "\n"
 results += str(dictionary.get("key2") == "value3") + " get changed value. Expected: value3 Actual: " + dictionary.get("key2") + "\n"
 results += str(not dictionary.isEmpty()) + " isEmpty. Expected: false Actual: " + str(dictionary.isEmpty()) + "\n"
 results += str(dictionary.size() == 2) + " size. Expected: 2 Actual: " + str(dictionary.size()) + "\n\n"
@@ -45,6 +45,7 @@ results += str(dictionary.size() == 2) + " size. Expected: 2 Actual: " + str(dic
 results += "\nRemove the 2 values from the dictionary, check isEmpty"
 dictionary.remove("key1")
 
+#check if it removed value, still contains not removed value, size, isEmpty
 results += str(not dictionary.contains("key1")) + " doesn't contain removed key. Expected false: Actual: " + str(dictionary.contains("key1")) + "\n"
 results += str(dictionary.contains("key2")) + " still contains added key. Expected: true Actual: " + str(dictionary.contains("key2")) + "\n"
 results += str(dictionary.get("key2") == "value3") + " get value. Expected: value3 Actual: " + str(dictionary.get("key2") + "\n")
@@ -52,7 +53,7 @@ results += str(not dictionary.isEmpty()) + " isEmpty for empty constructor. Expe
 results += str(dictionary.size() == 1) + " size. Expected: 1 Actual: " + str(dictionary.size()) + "\n"
 
 dictionary.remove("key2")
-
+#checked if it removed the value, size, isEmpty
 results += str(not dictionary.contains("key2")) + " doesn't contain removed key. Expected false: Actual: " + str(dictionary.contains("key2")) + "\n"
 results += str(dictionary.isEmpty()) + " isEmpty. Expected: true Actual: " + str(dictionary.isEmpty()) + "\n"
 results += str(dictionary.size() == 0) + " size. Expected: 0 Actual: " + str(dictionary.size()) + "\n\n"
@@ -64,14 +65,15 @@ dictionary.put("key1", "value1")
 dictionary.put("key2", "value2")
 dictionary.put("key3", "value3")
 
+#check if editing the copy will affect the instance data
 results += "\nGet keys and values list, modify lists and see if the instance data of the dictionary is modified.\n"
-keyList = dictionary.listKeys()
+keyList = dictionary.getKeys()
 keyList.remove("key1")
 results += str(dictionary.contains("key1")) + " still contains key. Expected: true Actual: " + str(dictionary.contains("key1")) + "\n"
 
-valuesList = dictionary.listValues()
+valuesList = dictionary.getValues()
 valuesList.remove("value1")
-values2 = dictionary.listValues()
+values2 = dictionary.getValues()
 results += str("value1" in values2) + " still contains value. Expected: true Actual: " + str(dictionary.contains("key1")) + "\n"
 
 
